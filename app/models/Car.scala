765d190d36afb721dfde42713a372e2739aacd95
Óscar López <olopez@tappsi.co>
2017-07-29 21:35:50 -0500

    Code fixes

--- app/models/Car.scala
+++ app/models/Car.scala
@@ -1,28 +1,28 @@
 package models
 
-class Car(val id: Int, val capacity: Int, var current: String, tm: TransmiMetro) {
+class Car(val id: Int, val capacity: Int, var currentStation: String, tm: TransmiMetro) {
 
   var passengers: List[Passenger] = List[Passenger]()
 
   def update(time: String): Unit = {
-    current = tm.getCarStop(this, time).departure
+    currentStation = tm.getCarStop(this, time).departure
   }
 
   // pre: the arrived list fits int car's capacity
   // pre: `current` was updated with the station the car is currently in
   def simulate(time: String, boardingPassengers: Iterable[Passenger]): Unit = {
 
-    val departures = passengers.count(_.destination == current)
-    tm.log(time, current, "departures", departures)
+    val departures = passengers.count(_.destination == currentStation)
+    tm.log(time, currentStation, "departures", departures)
 
-    passengers = passengers.filter(_.destination != current)
+    passengers = passengers.filter(_.destination != currentStation)
     passengers ++= boardingPassengers
 
   }
 
   // pre: `current` was updated with the station the car is currently in
   def availableSeats(): Int = {
-    val departures = passengers.count(_.destination == current)
+    val departures = passengers.count(_.destination == currentStation)
     capacity - departures
   }
 
--- app/models/Simulator.scala
+++ app/models/Simulator.scala
@@ -1,6 +1,5 @@
 package models
 
-
 import java.io.File
 import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
@@ -10,6 +9,7 @@ class Simulator {
   val stations_dir = "/Users/jorgegrajales/Repos/transmi-metro/data/stations"
 
   def getStationList(): List[String] = {
+
     def getListOfFiles(dir: String): List[File] = {
       val d = new File(dir)
       if (d.exists && d.isDirectory) {
@@ -19,14 +19,16 @@ class Simulator {
       }
     }
 
-    val stations_files = getListOfFiles(stations_dir).map(x => x.toString)
+    getListOfFiles(stations_dir).map(x => x.toString)
       .map(x => x.substring(0, x.lastIndexOf('.'))).map(x => x.substring(x.lastIndexOf("/") + 1))
-    return stations_files
+
   }
 
   def getPassengers(station: String): List[Passenger] = {
+
     val rows = ArrayBuffer[Array[String]]()
     var passengers = List[Passenger]()
+
     using(Source.fromFile(stations_dir + "/" + station + ".csv")) { source =>
       for (line <- source.getLines.drop(1)) {
         rows += line.split(",").map(_.trim)
@@ -37,18 +39,21 @@ class Simulator {
       val passengerId = row(0).toInt
       val time = row(1)
       val destination = row(2)
-      passengers :+ new Passenger(passengerId, destination)
+      passengers = new Passenger(passengerId, destination) +: passengers
     }
 
-    def using[A <: {def close() : Unit}, B](resource: A)(f: A => B): B =
-      try {
-        f(resource)
-      } finally {
-        resource.close()
-      }
+    passengers
 
-    return passengers
   }
 
+  // private helpers
+
+  private def using[A <: {def close() : Unit}, B](resource: A)(f: A => B): B = {
+    try {
+      f(resource)
+    } finally {
+      resource.close()
+    }
+  }
 
 }
